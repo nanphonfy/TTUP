f90970c15ad486aaa976ec02ba39b37f8a2907bd
nanphonfy <nanphonfy@gmail.com>
2018-03-06 23:52:03 +0800

    抽象工厂模式v1.0
    前两次写错

---
+++ design-patterns/src/main/java/_09abstract_factory/section1/MaleYellowHuman.java
@@ -0,0 +1,14 @@
+package _09abstract_factory.section1;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ */
+public class MaleYellowHuman extends AbstractYellowHuman {
+
+	//黄人男性
+	@Override public void getSex() {
+		System.out.println("黄人男性");
+	}
+
+}
